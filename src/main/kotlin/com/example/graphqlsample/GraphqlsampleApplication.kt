6d6f84bfd8461c3678349169c914312ce3077006
hiro21 <hiswing8@gmail.com>
2020-09-17 19:30:16 +0900

    first commit

---
+++ src/main/kotlin/com/example/graphqlsample/GraphqlsampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.graphqlsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphqlsampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<GraphqlsampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/graphqlsample/GraphqlsampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.graphqlsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GraphqlsampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
